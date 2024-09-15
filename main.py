6ff64b1415bc375c2066a38d743ae571325e7e9b
AlexBoniske <alexboniske1@gmail.com>
2024-09-14 20:33:07 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import CheckScheduleRelated
+import CheckFree
+import RespondToEmail
+import authGoogleCal
+import createEvent
+import getCalendarDates
+
+def main():
+    print("hello")
+
+if __name__ == '__main__':
+    pass
